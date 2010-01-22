b40acb454dc0c02c6410bfcb4d0bb46a3b2cee4f
Pete Muir <pmuir@bleepbleep.org.uk>
2010-01-22 16:38:38 +0000

    port r5422 to trunk

--- impl/src/main/java/org/jboss/jsr299/tck/tests/context/dependent/DependentContextTest.java
+++ impl/src/main/java/org/jboss/jsr299/tck/tests/context/dependent/DependentContextTest.java
@@ -189,9 +189,8 @@ public class DependentContextTest extends AbstractJSR299Test
    })
    public void testContextIsActiveWhenInvokingProducerMethod()
    {
-      Bean<Tarantula> tarantulaBean = getBeans(Tarantula.class).iterator().next();
-      CreationalContext<Tarantula> creationalContext = getCurrentManager().createCreationalContext(tarantulaBean);
-      Tarantula tarantula = tarantulaBean.create(creationalContext);
+      SpiderProducer.reset();
+      Tarantula tarantula = getInstanceByType(Tarantula.class,PET_LITERAL);
       assert tarantula != null;
       assert SpiderProducer.isDependentContextActive();
       SpiderProducer.reset();
@@ -205,6 +204,7 @@ public class DependentContextTest extends AbstractJSR299Test
       Tarantula.reset();
       getInstanceByType(Tarantula.class,TAME_LITERAL);
       assert Tarantula.isDependentContextActive();
+      SpiderProducer.reset();
    }
 
    @Test(groups = { "contexts", "disposalMethod" })
