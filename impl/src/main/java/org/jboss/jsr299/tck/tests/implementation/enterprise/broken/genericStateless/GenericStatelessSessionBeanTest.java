b71d14eaa41557721d6c4e0105bd76eab941a234
Jozef Hartinger <jharting@redhat.com>
2010-02-09 09:38:01 +0000

    Definition error detection tests for EJBs

---
+++ impl/src/main/java/org/jboss/jsr299/tck/tests/implementation/enterprise/broken/genericStateless/GenericStatelessSessionBeanTest.java
@@ -0,0 +1,43 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2010, Red Hat, Inc., and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,  
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jboss.jsr299.tck.tests.implementation.enterprise.broken.genericStateless;
+
+
+import org.jboss.jsr299.tck.AbstractJSR299Test;
+import org.jboss.jsr299.tck.DeploymentFailure;
+import org.jboss.test.audit.annotations.SpecAssertion;
+import org.jboss.test.audit.annotations.SpecVersion;
+import org.jboss.testharness.impl.packaging.Artifact;
+import org.jboss.testharness.impl.packaging.ExpectedDeploymentException;
+import org.jboss.testharness.impl.packaging.Packaging;
+import org.jboss.testharness.impl.packaging.PackagingType;
+import org.testng.annotations.Test;
+
+@Artifact
+@Packaging(PackagingType.EAR)
+@ExpectedDeploymentException(DeploymentFailure.class)
+@SpecVersion(spec="cdi", version="20091101")
+public class GenericStatelessSessionBeanTest extends AbstractJSR299Test
+{
+   @Test(groups = { "enterpriseBeans" })
+   @SpecAssertion(section = "3.2", id = "i")
+   //WELD-433
+   public void testRequestScopedGenericStatelessSessionBean()
+   {
+      assert false;
+   }
+}
