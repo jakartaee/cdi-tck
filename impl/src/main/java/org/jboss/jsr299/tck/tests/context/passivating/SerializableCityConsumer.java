4acaec9741dbb76fbccf7a3cea12ce65bb25a943
Pete Muir <pmuir@bleepbleep.org.uk>
2010-02-20 18:51:28 +0000

    CDITCK-113

--- impl/src/main/java/org/jboss/jsr299/tck/tests/context/passivating/SerializableCityConsumer.java
+++ impl/src/main/java/org/jboss/jsr299/tck/tests/context/passivating/SerializableCityConsumer.java
@@ -19,7 +19,7 @@ package org.jboss.jsr299.tck.tests.context.passivating;
 import javax.enterprise.context.RequestScoped;
 
 @RequestScoped
-class SerializableCityConsumer
+public class SerializableCityConsumer
 {
 
    @Sleeping SerializableCity city;
--- impl/src/main/java/org/jboss/jsr299/tck/tests/context/passivating/Television.java
+++ impl/src/main/java/org/jboss/jsr299/tck/tests/context/passivating/Television.java
@@ -19,7 +19,7 @@ package org.jboss.jsr299.tck.tests.context.passivating;
 import javax.enterprise.inject.Alternative;
 
 @Alternative
-class Television
+public class Television
 {
    public void turnOn() {}
 }
