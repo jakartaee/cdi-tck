081af7ea275ed72661f10d0073cf204c0843e144
Pete Muir <pmuir@bleepbleep.org.uk>
2010-12-07 17:54:13 +0000

    CDITCK-190

--- impl/src/main/java/org/jboss/jsr299/tck/tests/event/observer/enterprise/LazyFarmer.java
+++ impl/src/main/java/org/jboss/jsr299/tck/tests/event/observer/enterprise/LazyFarmer.java
@@ -18,6 +18,6 @@ package org.jboss.jsr299.tck.tests.event.observer.enterprise;
 
 import javax.ejb.Stateful;
 
-public @Stateful class LazyFarmer extends Farmer implements LazyFarmerLocal
+public @Stateful class LazyFarmer extends Farmer implements LazyFarmerLocal,FarmerLocal
 {
 }
