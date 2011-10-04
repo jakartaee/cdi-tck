56967d70f3aa2b48282d41ab1666175369bbcdab
Martin Kouba <mkouba@redhat.com>
2011-10-04 15:57:49 +0200

    New tests covering invalid restrictions of bean types

---
+++ impl/src/main/java/org/jboss/jsr299/tck/tests/definition/bean/broken/restricted/RestrictedProducerFieldTest.java
@@ -0,0 +1,47 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2011, Red Hat, Inc., and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,  
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jboss.jsr299.tck.tests.definition.bean.broken.restricted;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.container.test.api.ShouldThrowException;
+import org.jboss.jsr299.tck.AbstractJSR299Test;
+import org.jboss.jsr299.tck.shrinkwrap.WebArchiveBuilder;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.jboss.test.audit.annotations.SpecAssertion;
+import org.jboss.test.audit.annotations.SpecVersion;
+import org.testng.annotations.Test;
+
+/**
+ * 
+ * @author Martin Kouba
+ */
+@SpecVersion(spec = "cdi", version = "20091101")
+public class RestrictedProducerFieldTest extends AbstractJSR299Test {
+
+    @ShouldThrowException(Exception.class)
+    @Deployment
+    public static WebArchive createTestArchive() {
+        return new WebArchiveBuilder().withTestClassDefinition(RestrictedProducerFieldTest.class)
+                .withClasses(Animal.class, Boulder.class, BoulderFieldProducer.class).build();
+    }
+
+    @Test
+    @SpecAssertion(section = "2.2.2", id = "l")
+    public void testInvalidTypedValueOnProducerField() {
+    }
+
+}
