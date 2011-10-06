98334785d779b9c48c82e4082319bc688b63c4e0
Martin Kouba <mkouba@redhat.com>
2011-10-06 10:30:10 +0200

    Seam Compatibility tests - iteration 1 / CDITCK-230

---
+++ impl/src/main/java/org/jboss/jsr299/tck/tests/event/observer/broken/validation/ObserverMethodParameterInjectionValidationTest.java
@@ -0,0 +1,51 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2011, Red Hat, Inc. and/or its affiliates, and individual
+ * contributors by the @authors tag. See the copyright.txt in the
+ * distribution for a full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jboss.jsr299.tck.tests.event.observer.broken.validation;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.container.test.api.ShouldThrowException;
+import org.jboss.jsr299.tck.AbstractJSR299Test;
+import org.jboss.jsr299.tck.shrinkwrap.WebArchiveBuilder;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.jboss.test.audit.annotations.SpecAssertion;
+import org.jboss.test.audit.annotations.SpecVersion;
+import org.testng.annotations.Test;
+
+/**
+ * Validates that injection points on observer methods are validated.
+ * 
+ * <p>
+ * This test was originally part of Seam Compatibility project.
+ * <p>
+ * 
+ * @author <a href="http://community.jboss.org/people/dan.j.allen">Dan Allen</a>
+ * @author Martin Kouba
+ */
+@SpecVersion(spec = "cdi", version = "20091101")
+public class ObserverMethodParameterInjectionValidationTest extends AbstractJSR299Test {
+
+    @ShouldThrowException(Exception.class)
+    @Deployment
+    public static WebArchive getDeployment() {
+        return new WebArchiveBuilder().withTestClassPackage(ObserverMethodParameterInjectionValidationTest.class).build();
+    }
+
+    @Test
+    @SpecAssertion(section = "10.4.2", id = "i")
+    public void test() {
+    }
+}
---
+++ impl/src/main/java/org/jboss/jsr299/tck/tests/extensions/alternative/deployment/AlternativeInLibraryWithExtensionTest.java
@@ -0,0 +1,67 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2011, Red Hat, Inc. and/or its affiliates, and individual
+ * contributors by the @authors tag. See the copyright.txt in the
+ * distribution for a full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jboss.jsr299.tck.tests.extensions.alternative.deployment;
+
+import javax.inject.Inject;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.jsr299.tck.AbstractJSR299Test;
+import org.jboss.jsr299.tck.shrinkwrap.WebArchiveBuilder;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.jboss.shrinkwrap.descriptor.api.Descriptors;
+import org.jboss.shrinkwrap.descriptor.api.beans10.BeansDescriptor;
+import org.jboss.test.audit.annotations.SpecAssertion;
+import org.jboss.test.audit.annotations.SpecAssertions;
+import org.jboss.test.audit.annotations.SpecVersion;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/**
+ * TODO check assertions since this test is not aimed to specific assertion (is a result of practical issue)
+ * 
+ * This test verifies that alternatives work correctly in BDAs that contain at least one extension.
+ * 
+ * <p>
+ * This test was originally part of Seam Compatibility project.
+ * <p>
+ * 
+ * @author <a href="http://community.jboss.org/people/jharting">Jozef Hartinger</a>
+ * @author Martin Kouba
+ * @see <a href="http://java.net/jira/browse/GLASSFISH-15791">GLASSFISH-15791</a>
+ */
+@SpecVersion(spec = "cdi", version = "20091101")
+public class AlternativeInLibraryWithExtensionTest extends AbstractJSR299Test {
+
+    @Inject
+    private Foo foo;
+
+    @Deployment
+    public static WebArchive createTestArchive() {
+        return new WebArchiveBuilder()
+                .withTestClass(AlternativeInLibraryWithExtensionTest.class)
+                .withBeanLibrary(
+                        Descriptors.create(BeansDescriptor.class).createAlternatives().clazz(BarAlternative.class.getName())
+                                .up(), Foo.class, Bar.class, BarAlternative.class, NoopExtension.class).build();
+    }
+
+    @Test
+    @SpecAssertions({ @SpecAssertion(section = "2.6", id = "a"), @SpecAssertion(section = "2.6.1", id = "a"),
+            @SpecAssertion(section = "12.2", id = "b") })
+    public void testAlternative() {
+        Assert.assertEquals("barAlternative", foo.getBar().ping());
+    }
+}
