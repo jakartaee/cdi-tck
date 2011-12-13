85fac44662d95c429cc07edc7aecc57e87e75e5b
Martin Kouba <mkouba@redhat.com>
2011-12-13 14:52:02 +0100

    CDITCK-250 fix - missing injection point for WrappedEnterpriseBean

--- impl/src/main/java/org/jboss/jsr299/tck/tests/implementation/enterprise/newBean/NewSessionBeanConsumer.java
+++ impl/src/main/java/org/jboss/jsr299/tck/tests/implementation/enterprise/newBean/NewSessionBeanConsumer.java
@@ -52,4 +52,8 @@ public class NewSessionBeanConsumer {
     @New(ExplicitConstructorSessionBean.class)
     ExplicitConstructor explicitConstructor;
 
+    @Inject
+    @New(WrappedEnterpriseBean.class)
+    WrappedEnterpriseBeanLocal wrappedEnterpriseBean;
+
 }
