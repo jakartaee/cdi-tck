8cce3f19ddfba156ea121c55e7baba352d9e39f8
Martin Kouba <mkouba@redhat.com>
2012-10-10 10:28:52 +0200

    New test - stereotype with alternative for session bean

--- impl/src/main/java/org/jboss/cdi/tck/tests/definition/stereotype/alternative/enterprise/EnterpriseStereotypeAlternativeSpecializeTest.java
+++ impl/src/main/java/org/jboss/cdi/tck/tests/definition/stereotype/alternative/enterprise/EnterpriseStereotypeAlternativeSpecializeTest.java
@@ -1,6 +1,6 @@
 /*
  * JBoss, Home of Professional Open Source
- * Copyright 2010, Red Hat, Inc., and individual contributors
+ * Copyright 2012, Red Hat, Inc., and individual contributors
  * by the @authors tag. See the copyright.txt in the distribution for a
  * full listing of individual contributors.
  *
@@ -9,16 +9,17 @@
  * You may obtain a copy of the License at
  * http://www.apache.org/licenses/LICENSE-2.0
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,  
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.jboss.cdi.tck.tests.definition.stereotype.interceptor.enterprise;
+
+package org.jboss.cdi.tck.tests.definition.stereotype.alternative.enterprise;
 
 import static org.jboss.cdi.tck.TestGroups.INTEGRATION;
-
-import javax.inject.Inject;
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertNotNull;
 
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.cdi.tck.AbstractTest;
@@ -29,35 +30,31 @@ import org.jboss.shrinkwrap.descriptor.api.beans10.BeansDescriptor;
 import org.jboss.test.audit.annotations.SpecAssertion;
 import org.jboss.test.audit.annotations.SpecAssertions;
 import org.jboss.test.audit.annotations.SpecVersion;
-import org.testng.Assert;
 import org.testng.annotations.Test;
 
 /**
- * 
  * @author Martin Kouba
+ * 
  */
 @Test(groups = INTEGRATION)
 @SpecVersion(spec = "cdi", version = "20091101")
-public class EnterpriseStereotypeInterceptorBindingTest extends AbstractTest {
+public class EnterpriseStereotypeAlternativeSpecializeTest extends AbstractTest {
 
     @Deployment
     public static WebArchive createTestArchive() {
         return new WebArchiveBuilder()
-                .withTestClassPackage(EnterpriseStereotypeInterceptorBindingTest.class)
+                .withTestClass(EnterpriseStereotypeAlternativeSpecializeTest.class)
+                .withClasses(Mock.class, Service.class, MockService.class, RealService.class, AbstractService.class)
                 .withBeansXml(
-                        Descriptors.create(BeansDescriptor.class).createInterceptors().clazz(AlphaInterceptor.class.getName())
-                                .up()).build();
+                        Descriptors.create(BeansDescriptor.class).createAlternatives().stereotype(Mock.class.getName()).up())
+                .build();
     }
 
-    @Inject
-    Foo foo;
-
-    @Test
-    @SpecAssertions({ @SpecAssertion(section = "2.7.1.2", id = "b") })
-    public void testStereotypeInterceptorBindings() {
-        AlphaInterceptor.reset();
-        foo.ping();
-        Assert.assertTrue(AlphaInterceptor.intercepted);
+    @Test(dataProvider = ARQUILLIAN_DATA_PROVIDER)
+    @SpecAssertions({ @SpecAssertion(section = "2.7.1.4", id = "a") })
+    public void testStereotypeAlternative(Service service) {
+        assertNotNull(service);
+        assertEquals(service.ping(), MockService.class.getName());
     }
 
 }
