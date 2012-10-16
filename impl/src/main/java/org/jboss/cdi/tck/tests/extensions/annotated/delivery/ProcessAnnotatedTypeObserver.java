161a09e6b75f0b5e5008f4372a8df550e832b3bc
Martin Kouba <mkouba@redhat.com>
2012-10-16 15:58:07 +0200

    CDI-43

---
+++ impl/src/main/java/org/jboss/cdi/tck/tests/extensions/annotated/delivery/ProcessAnnotatedTypeObserver.java
@@ -0,0 +1,59 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2010, Red Hat, Inc., and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,  
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jboss.cdi.tck.tests.extensions.annotated.delivery;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import javax.enterprise.event.Observes;
+import javax.enterprise.inject.spi.Extension;
+import javax.enterprise.inject.spi.ProcessAnnotatedType;
+import javax.enterprise.inject.spi.WithAnnotations;
+import javax.inject.Qualifier;
+
+public class ProcessAnnotatedTypeObserver implements Extension {
+
+    private List<Class<?>> processedDesiredTypes = new ArrayList<Class<?>>();
+    private List<Class<?>> processedDesiredAndWantedTypes = new ArrayList<Class<?>>();
+    private List<Class<?>> processedMetaAnnotationTypes = new ArrayList<Class<?>>();
+
+    public void observeDesiredTypes(@WithAnnotations({ Desired.class }) @Observes ProcessAnnotatedType<?> pat) {
+        processedDesiredTypes.add(pat.getAnnotatedType().getJavaClass());
+    }
+
+    public void observeDesiredAndWanted(@WithAnnotations({ Desired.class, Wanted.class }) @Observes ProcessAnnotatedType<?> pat) {
+        processedDesiredAndWantedTypes.add(pat.getAnnotatedType().getJavaClass());
+    }
+
+    public void observeMetaAnnotationTypes(@WithAnnotations({ Qualifier.class }) @Observes ProcessAnnotatedType<?> pat) {
+        processedMetaAnnotationTypes.add(pat.getAnnotatedType().getJavaClass());
+    }
+
+    public List<Class<?>> getProcessedDesiredTypes() {
+        return Collections.unmodifiableList(processedDesiredTypes);
+    }
+
+    public List<Class<?>> getProcessedDesiredAndWantedTypes() {
+        return Collections.unmodifiableList(processedDesiredAndWantedTypes);
+    }
+
+    public List<Class<?>> getProcessedMetaAnnotationTypes() {
+        return Collections.unmodifiableList(processedMetaAnnotationTypes);
+    }
+
+}
---
+++ impl/src/main/java/org/jboss/cdi/tck/tests/extensions/annotated/delivery/WithAnnotationsTest.java
@@ -0,0 +1,90 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2012, Red Hat, Inc., and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.jboss.cdi.tck.tests.extensions.annotated.delivery;
+
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertFalse;
+import static org.testng.Assert.assertTrue;
+
+import java.util.List;
+
+import javax.inject.Inject;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.cdi.tck.AbstractTest;
+import org.jboss.cdi.tck.shrinkwrap.WebArchiveBuilder;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.jboss.test.audit.annotations.SpecAssertion;
+import org.jboss.test.audit.annotations.SpecAssertions;
+import org.jboss.test.audit.annotations.SpecVersion;
+import org.testng.annotations.Test;
+
+/**
+ * @author Martin Kouba
+ * 
+ */
+@SpecVersion(spec = "cdi", version = "20091101")
+public class WithAnnotationsTest extends AbstractTest {
+
+    @SuppressWarnings("unchecked")
+    @Deployment
+    public static WebArchive createTestArchive() {
+        return new WebArchiveBuilder().withTestClassPackage(WithAnnotationsTest.class)
+                .withExtensions(ProcessAnnotatedTypeObserver.class, BeforeBeanDiscoveryObserver.class).build();
+    }
+
+    @Inject
+    ProcessAnnotatedTypeObserver processAnnotatedTypeObserver;
+
+    @Test
+    @SpecAssertions({ @SpecAssertion(section = "11.5.6", id = "fa"), @SpecAssertion(section = "11.5.6", id = "fb"),
+            @SpecAssertion(section = "11.5.6", id = "fc"), @SpecAssertion(section = "11.5.6", id = "fd"),
+            @SpecAssertion(section = "11.5.6", id = "fe"), @SpecAssertion(section = "11.5.6", id = "ff") })
+    public void testDelivery() {
+
+        List<Class<?>> processedDesiredAnWantedTypes = processAnnotatedTypeObserver.getProcessedDesiredAndWantedTypes();
+        assertFalse(processedDesiredAnWantedTypes.isEmpty());
+        assertEquals(processedDesiredAnWantedTypes.size(), 7);
+        assertTrue(processedDesiredAnWantedTypes.contains(Hummingbird.class));
+        assertTrue(processedDesiredAnWantedTypes.contains(Falcon.class));
+        assertTrue(processedDesiredAnWantedTypes.contains(Turkey.class));
+        assertTrue(processedDesiredAnWantedTypes.contains(Sparrow.class));
+        assertTrue(processedDesiredAnWantedTypes.contains(Hen.class));
+        assertTrue(processedDesiredAnWantedTypes.contains(Hawk.class));
+        assertTrue(processedDesiredAnWantedTypes.contains(Phoenix.class));
+
+        List<Class<?>> processedDesiredTypes = processAnnotatedTypeObserver.getProcessedDesiredTypes();
+        assertFalse(processedDesiredTypes.isEmpty());
+        assertEquals(processedDesiredTypes.size(), 4);
+        assertTrue(processedDesiredTypes.contains(Hummingbird.class));
+        assertTrue(processedDesiredTypes.contains(Turkey.class));
+        assertTrue(processedDesiredTypes.contains(Sparrow.class));
+        assertTrue(processedDesiredTypes.contains(Hawk.class));
+    }
+
+    @Test
+    @SpecAssertions({ @SpecAssertion(section = "11.5.6", id = "fa"), @SpecAssertion(section = "11.5.6", id = "g") })
+    public void testDeliveryMetaAnnotation() {
+        List<Class<?>> processedTypes = processAnnotatedTypeObserver.getProcessedMetaAnnotationTypes();
+        assertFalse(processedTypes.isEmpty());
+        assertEquals(processedTypes.size(), 2);
+        assertTrue(processedTypes.contains(Chicken.class));
+        assertTrue(processedTypes.contains(Hummingbird.class));
+    }
+
+}
