2b4cc69637a8fa3801cc1777c55b34fe8b68baee
Martin Kouba <mkouba@redhat.com>
2012-11-20 15:57:04 +0100

    Add tck specific assert util class

---
+++ impl/src/main/java/org/jboss/cdi/tck/util/Assert.java
@@ -0,0 +1,61 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2012, Red Hat, Inc., and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.jboss.cdi.tck.util;
+
+import static org.testng.Assert.fail;
+
+import java.lang.annotation.Annotation;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * @author Martin Kouba
+ * 
+ */
+public class Assert {
+
+    private Assert() {
+    }
+
+    /**
+     * 
+     * @param annotations
+     * @param requiredAnnotationTypes
+     * @throws AssertionError if the annotations set and required annotations do not match
+     */
+    public static void assertAnnotationSetMatches(Set<? extends Annotation> annotations,
+            Class<? extends Annotation>... requiredAnnotationTypes) {
+
+        if (annotations.size() != requiredAnnotationTypes.length) {
+            fail("Set " + annotations.toString() + " does not match array " + Arrays.toString(requiredAnnotationTypes));
+        }
+
+        if (annotations.isEmpty() && requiredAnnotationTypes.length == 0) {
+            return;
+        }
+
+        List<Class<? extends Annotation>> requiredAnnotationTypesList = Arrays.asList(requiredAnnotationTypes);
+
+        for (Annotation annotation : annotations) {
+            if (!requiredAnnotationTypesList.contains(annotation.annotationType())) {
+                fail("Set " + annotations.toString() + " does not match array " + Arrays.toString(requiredAnnotationTypes));
+            }
+        }
+    }
+}
---
+++ impl/src/test/java/org/jboss/cdi/tck/test/util/AssertTest.java
@@ -0,0 +1,92 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2012, Red Hat, Inc., and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.jboss.cdi.tck.test.util;
+
+import java.lang.annotation.Annotation;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.enterprise.inject.Any;
+import javax.enterprise.inject.Default;
+import javax.inject.Inject;
+
+import org.jboss.cdi.tck.literals.AnyLiteral;
+import org.jboss.cdi.tck.literals.InjectLiteral;
+import org.jboss.cdi.tck.util.Assert;
+import org.testng.annotations.Test;
+
+/**
+ * @author Martin Kouba
+ * 
+ */
+public class AssertTest {
+
+    @SuppressWarnings("unchecked")
+    @Test
+    public void testAnnotationSetMatches() {
+        Set<Annotation> annotations = new HashSet<Annotation>();
+        annotations.add(AnyLiteral.INSTANCE);
+        annotations.add(InjectLiteral.INSTANCE);
+        Assert.assertAnnotationSetMatches(annotations, Any.class, Inject.class);
+    }
+
+    @SuppressWarnings("unchecked")
+    @Test(expectedExceptions = AssertionError.class)
+    public void testAnnotationSetDoesNotMatchA() {
+        Set<Annotation> annotations = new HashSet<Annotation>();
+        annotations.add(AnyLiteral.INSTANCE);
+        annotations.add(InjectLiteral.INSTANCE);
+        Assert.assertAnnotationSetMatches(annotations, Any.class);
+    }
+
+    @SuppressWarnings("unchecked")
+    @Test(expectedExceptions = AssertionError.class)
+    public void testAnnotationSetDoesNotMatchB() {
+        Set<Annotation> annotations = new HashSet<Annotation>();
+        annotations.add(InjectLiteral.INSTANCE);
+        Assert.assertAnnotationSetMatches(annotations, Any.class, Inject.class);
+    }
+
+    @SuppressWarnings("unchecked")
+    @Test(expectedExceptions = AssertionError.class)
+    public void testAnnotationSetDoesNotMatchC() {
+        Set<Annotation> annotations = new HashSet<Annotation>();
+        annotations.add(AnyLiteral.INSTANCE);
+        annotations.add(InjectLiteral.INSTANCE);
+        Assert.assertAnnotationSetMatches(annotations, Any.class, Default.class);
+    }
+
+    @SuppressWarnings("unchecked")
+    @Test
+    public void testAnnotationSetIsEmptyAndRequiredAnnotationsEmpty() {
+        Assert.assertAnnotationSetMatches(new HashSet<Annotation>());
+    }
+
+    @SuppressWarnings("unchecked")
+    @Test(expectedExceptions = AssertionError.class)
+    public void testAnnotationSetIsEmpty() {
+        Assert.assertAnnotationSetMatches(new HashSet<Annotation>(), Any.class);
+    }
+
+    @SuppressWarnings("unchecked")
+    @Test(expectedExceptions = NullPointerException.class)
+    public void testAnnotationSetIsNull() {
+        Assert.assertAnnotationSetMatches(null);
+    }
+
+}
