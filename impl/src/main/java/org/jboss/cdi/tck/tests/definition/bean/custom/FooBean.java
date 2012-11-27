310fb9bd28e728931b19574733fa15ac2c33c28f
Jozef Hartinger <jharting@redhat.com>
2012-11-27 12:02:36 +0100

    FooBean has a passivating scope, it should be PassivationCapable

--- impl/src/main/java/org/jboss/cdi/tck/tests/definition/bean/custom/FooBean.java
+++ impl/src/main/java/org/jboss/cdi/tck/tests/definition/bean/custom/FooBean.java
@@ -9,7 +9,7 @@
  * You may obtain a copy of the License at
  * http://www.apache.org/licenses/LICENSE-2.0
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,  
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
@@ -27,10 +27,11 @@ import javax.enterprise.context.spi.CreationalContext;
 import javax.enterprise.inject.spi.AnnotatedField;
 import javax.enterprise.inject.spi.Bean;
 import javax.enterprise.inject.spi.InjectionPoint;
+import javax.enterprise.inject.spi.PassivationCapable;
 
 import org.jboss.cdi.tck.literals.DefaultLiteral;
 
-public class FooBean implements Bean<Foo> {
+public class FooBean implements Bean<Foo>, PassivationCapable {
 
     public static CustomInjectionPoint integerInjectionPoint;
 
@@ -100,6 +101,11 @@ public class FooBean implements Bean<Foo> {
         injectionPoints.add(integerInjectionPoint);
         injectionPoints.add(barInjectionPoint);
         return injectionPoints;
+    }
+
+    @Override
+    public String getId() {
+        return AfterBeanDiscoveryObserver.class.getName() + ":" + FooBean.class.getName();
     }
 
 }
