6ed55455044b7374ded69e153c9df81afac6655f
Martin Kouba <mkouba@redhat.com>
2012-12-12 13:26:07 +0100

    New tests - validation of decorators

---
+++ impl/src/main/java/org/jboss/cdi/tck/tests/decorators/custom/broken/toomanydelegateinjectionpoints/AfterBeanDiscoveryObserver.java
@@ -0,0 +1,50 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2010, Red Hat, Inc., and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,  
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jboss.cdi.tck.tests.decorators.custom.broken.toomanydelegateinjectionpoints;
+
+import java.util.Set;
+
+import javax.enterprise.event.Observes;
+import javax.enterprise.inject.spi.AfterBeanDiscovery;
+import javax.enterprise.inject.spi.AnnotatedField;
+import javax.enterprise.inject.spi.AnnotatedType;
+import javax.enterprise.inject.spi.BeanManager;
+import javax.enterprise.inject.spi.Extension;
+import javax.enterprise.inject.spi.ProcessAnnotatedType;
+
+public class AfterBeanDiscoveryObserver implements Extension {
+
+    private static CustomDecorator decorator;
+
+    public void observeAfterBeanDiscovery(@Observes AfterBeanDiscovery event, BeanManager beanManager) {
+
+        AnnotatedType<VehicleDecorator> type = beanManager.createAnnotatedType(VehicleDecorator.class);
+        Set<AnnotatedField<? super VehicleDecorator>> annotatedFields = type.getFields();
+        AnnotatedField<? super VehicleDecorator> annotatedField = annotatedFields.iterator().next();
+        decorator = new CustomDecorator(annotatedField, beanManager);
+
+        event.addBean(decorator);
+    }
+
+    public void vetoVehicleDecorator(@Observes ProcessAnnotatedType<VehicleDecorator> event) {
+        event.veto();
+    }
+
+    public static CustomDecorator getDecorator() {
+        return decorator;
+    }
+}
