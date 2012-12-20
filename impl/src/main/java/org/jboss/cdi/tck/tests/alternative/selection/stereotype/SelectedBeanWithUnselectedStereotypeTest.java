3150582067aaf0d3a71942378ebbd35609e40165
Martin Kouba <mkouba@redhat.com>
2012-12-20 12:50:31 +0100

    New test - selected bean with unselected stereotype

---
+++ impl/src/main/java/org/jboss/cdi/tck/tests/alternative/selection/stereotype/SelectedBeanWithUnselectedStereotypeTest.java
@@ -0,0 +1,62 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2012, Red Hat, Inc., and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.jboss.cdi.tck.tests.alternative.selection.stereotype;
+
+import static org.testng.Assert.assertEquals;
+
+import java.util.Set;
+
+import javax.enterprise.context.RequestScoped;
+import javax.enterprise.inject.spi.Bean;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.cdi.tck.AbstractTest;
+import org.jboss.cdi.tck.shrinkwrap.WebArchiveBuilder;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.jboss.shrinkwrap.descriptor.api.Descriptors;
+import org.jboss.shrinkwrap.descriptor.api.beans10.BeansDescriptor;
+import org.jboss.test.audit.annotations.SpecAssertion;
+import org.jboss.test.audit.annotations.SpecAssertions;
+import org.jboss.test.audit.annotations.SpecVersion;
+import org.testng.annotations.Test;
+
+/**
+ * @author Martin Kouba
+ */
+@SpecVersion(spec = "cdi", version = "20091101")
+public class SelectedBeanWithUnselectedStereotypeTest extends AbstractTest {
+
+    @Deployment
+    public static WebArchive createTestArchive() {
+        return new WebArchiveBuilder().withTestClassPackage(SelectedBeanWithUnselectedStereotypeTest.class)
+                .withBeansXml(Descriptors.create(BeansDescriptor.class).createAlternatives().clazz(Bar.class.getName()).up())
+                .build();
+    }
+
+    @Test
+    @SpecAssertions({ @SpecAssertion(section = "5.1.1", id = "ca") })
+    public void testSingleAlternativeIsSelected() {
+        // Bar is selected
+        Set<Bean<Bar>> barBeans = getBeans(Bar.class);
+        assertEquals(barBeans.size(), 1);
+        assertEquals(barBeans.iterator().next().getScope(), RequestScoped.class);
+        // Foo is not selected
+        Set<Bean<Foo>> fooBeans = getBeans(Foo.class);
+        assertEquals(fooBeans.size(), 0);
+    }
+}
