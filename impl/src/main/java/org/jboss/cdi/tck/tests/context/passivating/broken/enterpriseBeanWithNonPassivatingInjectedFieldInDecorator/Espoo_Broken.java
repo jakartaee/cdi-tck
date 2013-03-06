2b1f7e74491ebf690e284a53990603c686a69f12
Jozef Hartinger <jharting@redhat.com>
2013-03-06 17:57:46 +0100

    Fix EnterpriseBeanWithNonPassivatingInjectedFieldInDecoratorTest

--- impl/src/main/java/org/jboss/cdi/tck/tests/context/passivating/broken/enterpriseBeanWithNonPassivatingInjectedFieldInDecorator/Espoo_Broken.java
+++ impl/src/main/java/org/jboss/cdi/tck/tests/context/passivating/broken/enterpriseBeanWithNonPassivatingInjectedFieldInDecorator/Espoo_Broken.java
@@ -9,7 +9,7 @@
  * You may obtain a copy of the License at
  * http://www.apache.org/licenses/LICENSE-2.0
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,  
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
@@ -19,11 +19,9 @@ package org.jboss.cdi.tck.tests.context.passivating.broken.enterpriseBeanWithNon
 import javax.ejb.Remove;
 import javax.ejb.Stateful;
 import javax.enterprise.context.SessionScoped;
-import javax.interceptor.Interceptors;
 
 @Stateful
 @SessionScoped
-@Interceptors(BrokenDecorator.class)
 public class Espoo_Broken implements EspooLocal_Broken {
     @Remove
     public void bye() {
