98057f4886aa3593010e06ad7aeac83215bc8353
Martin Kouba <mkouba@redhat.com>
2013-03-07 15:41:06 +0100

    CDITCK-323 New tests - AfterTypeDiscovery

---
+++ impl/src/main/java/org/jboss/cdi/tck/tests/extensions/lifecycle/atd/AfterTypeDiscoveryObserver.java
@@ -0,0 +1,78 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2013, Red Hat, Inc., and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jboss.cdi.tck.tests.extensions.lifecycle.atd;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import javax.enterprise.event.Observes;
+import javax.enterprise.inject.spi.AfterTypeDiscovery;
+import javax.enterprise.inject.spi.BeanManager;
+import javax.enterprise.inject.spi.Extension;
+import javax.enterprise.inject.spi.ProcessAnnotatedType;
+
+import org.jboss.cdi.tck.tests.extensions.lifecycle.atd.lib.Boss;
+
+/**
+ *
+ * @author Martin Kouba
+ */
+public class AfterTypeDiscoveryObserver implements Extension {
+
+    private List<Class<?>> interceptors = null;
+    private List<Class<?>> alternatives = null;
+    private List<Class<?>> decorators = null;
+    private boolean bossObserved = false;
+
+    public void observeAfterTypeDiscovery(@Observes AfterTypeDiscovery event, BeanManager beanManager) {
+
+        interceptors = Collections.unmodifiableList(new ArrayList<Class<?>>(event.getInterceptors()));
+        alternatives = Collections.unmodifiableList(new ArrayList<Class<?>>(event.getAlternatives()));
+        decorators = Collections.unmodifiableList(new ArrayList<Class<?>>(event.getDecorators()));
+
+        event.addAnnotatedType(beanManager.createAnnotatedType(Boss.class), AfterTypeDiscoveryObserver.class.getName());
+
+        // First interceptor removed
+        event.getInterceptors().remove(0);
+        // The order of decorators reverted
+        Collections.reverse(event.getDecorators());
+        // No selected alternative
+        event.getAlternatives().clear();
+    }
+
+    public void observeBossAnnotatedType(@Observes ProcessAnnotatedType<Boss> event) {
+        bossObserved = true;
+    }
+
+    public List<Class<?>> getInterceptors() {
+        return interceptors;
+    }
+
+    public List<Class<?>> getAlternatives() {
+        return alternatives;
+    }
+
+    public List<Class<?>> getDecorators() {
+        return decorators;
+    }
+
+    public boolean isBossObserved() {
+        return bossObserved;
+    }
+
+}
---
+++ impl/src/main/java/org/jboss/cdi/tck/tests/extensions/lifecycle/atd/AfterTypeDiscoveryTest.java
@@ -0,0 +1,121 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2013, Red Hat, Inc., and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jboss.cdi.tck.tests.extensions.lifecycle.atd;
+
+import static org.jboss.cdi.tck.cdi.Sections.ATD;
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertFalse;
+import static org.testng.Assert.assertTrue;
+
+import javax.inject.Inject;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.cdi.tck.AbstractTest;
+import org.jboss.cdi.tck.shrinkwrap.WebArchiveBuilder;
+import org.jboss.cdi.tck.tests.extensions.lifecycle.atd.lib.Boss;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.jboss.shrinkwrap.descriptor.api.Descriptors;
+import org.jboss.shrinkwrap.descriptor.api.beans10.BeansDescriptor;
+import org.jboss.test.audit.annotations.SpecAssertion;
+import org.jboss.test.audit.annotations.SpecAssertions;
+import org.jboss.test.audit.annotations.SpecVersion;
+import org.testng.annotations.Test;
+
+/**
+ *
+ * @author Martin Kouba
+ */
+@SpecVersion(spec = "cdi", version = "20091101")
+public class AfterTypeDiscoveryTest extends AbstractTest {
+
+    @Deployment
+    public static WebArchive createTestArchive() {
+        return new WebArchiveBuilder()
+                .withTestClassPackage(AfterTypeDiscoveryTest.class)
+                .withExtension(AfterTypeDiscoveryObserver.class)
+                .withLibrary(Boss.class)
+                .withBeansXml(
+                        Descriptors.create(BeansDescriptor.class).createInterceptors()
+                                .clazz(CharlieInterceptor.class.getName()).up().createDecorators()
+                                .clazz(CharlieDecorator.class.getName()).up().createAlternatives()
+                                .clazz(CharlieAlternative.class.getName()).up()).build();
+    }
+
+    @Inject
+    AfterTypeDiscoveryObserver extension;
+
+    @Test
+    @SpecAssertions({ @SpecAssertion(section = ATD, id = "a"), @SpecAssertion(section = ATD, id = "c"),
+            @SpecAssertion(section = ATD, id = "hb") })
+    public void testInitialInterceptors() {
+        assertEquals(extension.getInterceptors().size(), 2);
+        assertEquals(extension.getInterceptors().get(0), BravoInterceptor.class);
+        assertEquals(extension.getInterceptors().get(1), AlphaInterceptor.class);
+    }
+
+    @Test
+    @SpecAssertions({ @SpecAssertion(section = ATD, id = "b"), @SpecAssertion(section = ATD, id = "ha") })
+    public void testInitialAlternatives() {
+        assertEquals(extension.getAlternatives().size(), 2);
+        assertEquals(extension.getAlternatives().get(0), AlphaAlternative.class);
+        assertEquals(extension.getAlternatives().get(1), BravoAlternative.class);
+    }
+
+    @Test
+    @SpecAssertions({ @SpecAssertion(section = ATD, id = "d"), @SpecAssertion(section = ATD, id = "hc") })
+    public void testInitialDecorators() {
+        assertEquals(extension.getDecorators().size(), 2);
+        assertEquals(extension.getDecorators().get(0), AlphaDecorator.class);
+        assertEquals(extension.getDecorators().get(1), BravoDecorator.class);
+    }
+
+    @Test(dataProvider = ARQUILLIAN_DATA_PROVIDER)
+    @SpecAssertions({ @SpecAssertion(section = ATD, id = "gb") })
+    public void testFinalInterceptors(TransactionLogger logger) {
+
+        AlphaInterceptor.reset();
+        BravoInterceptor.reset();
+        CharlieInterceptor.reset();
+
+        logger.ping();
+
+        assertTrue(AlphaInterceptor.isIntercepted());
+        assertFalse(BravoInterceptor.isIntercepted());
+        assertTrue(CharlieInterceptor.isIntercepted());
+    }
+
+    @Test(dataProvider = ARQUILLIAN_DATA_PROVIDER)
+    @SpecAssertions({ @SpecAssertion(section = ATD, id = "gc") })
+    public void testFinalDecorators(TransactionLogger logger) {
+        assertEquals(logger.log("ping"), "pingbravoalphacharlie");
+    }
+
+    @Test
+    @SpecAssertions({ @SpecAssertion(section = ATD, id = "ga") })
+    public void testFinalAlternatives() {
+        assertTrue(getBeans(AlphaAlternative.class).isEmpty());
+        assertTrue(getBeans(BravoAlternative.class).isEmpty());
+    }
+
+    @Test
+    @SpecAssertions({ @SpecAssertion(section = ATD, id = "e") })
+    public void testAddAnnotatedType() {
+        extension.isBossObserved();
+        getUniqueBean(Boss.class);
+    }
+
+}
