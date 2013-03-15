2ad6a6a991802712d0cfc45537a91f705ae69949
Jozef Hartinger <jharting@redhat.com>
2013-03-15 09:56:38 +0100

    Fix WithAnnotationsTest - Baby should be @Inherited

--- impl/src/main/java/org/jboss/cdi/tck/tests/extensions/annotated/delivery/Baby.java
+++ impl/src/main/java/org/jboss/cdi/tck/tests/extensions/annotated/delivery/Baby.java
@@ -9,7 +9,7 @@
  * You may obtain a copy of the License at
  * http://www.apache.org/licenses/LICENSE-2.0
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,  
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
@@ -23,12 +23,14 @@ import static java.lang.annotation.ElementType.TYPE;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 import java.lang.annotation.Documented;
+import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
 @Target({ TYPE, METHOD, PARAMETER, FIELD })
 @Retention(RUNTIME)
 @Documented
+@Inherited
 @MetaAnnotation
 public @interface Baby {
 
