365b0b8c3d061b15c368cd4ceae31b6d284397cb
Martin Kouba <mkouba@redhat.com>
2013-03-22 16:27:08 +0100

    New tests - validation of passivation capable dependencies and @TransientReference

--- impl/src/main/java/org/jboss/cdi/tck/tests/context/passivating/broken/field/NonPassivatingInjectedFieldTest.java
+++ impl/src/main/java/org/jboss/cdi/tck/tests/context/passivating/broken/field/NonPassivatingInjectedFieldTest.java
@@ -9,7 +9,7 @@
  * You may obtain a copy of the License at
  * http://www.apache.org/licenses/LICENSE-2.0
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,  
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
@@ -30,13 +30,12 @@ import org.jboss.test.audit.annotations.SpecVersion;
 import org.testng.annotations.Test;
 
 @SpecVersion(spec = "cdi", version = "20091101")
-public class PassivatingManagedBeanWithNonPassivatingInjcetedFieldTest extends AbstractTest {
+public class NonPassivatingInjectedFieldTest extends AbstractTest {
 
     @ShouldThrowException(DeploymentException.class)
     @Deployment
     public static WebArchive createTestArchive() {
-        return new WebArchiveBuilder().withTestClassPackage(PassivatingManagedBeanWithNonPassivatingInjcetedFieldTest.class)
-                .build();
+        return new WebArchiveBuilder().withTestClassPackage(NonPassivatingInjectedFieldTest.class).build();
     }
 
     @Test
