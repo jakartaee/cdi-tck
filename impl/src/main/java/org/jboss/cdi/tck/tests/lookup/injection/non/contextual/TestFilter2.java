1a0b13ef6e03f76e104cbf6d52d8d4788ac263c3
Matus Abaffy <maabaffy@redhat.com>
2013-08-06 15:42:07 +0200

    Third part of CDITCK-345 - Add tests for EE component setter injection - @WebResourceRef tested on servlet and servlet filter

---
+++ impl/src/main/java/org/jboss/cdi/tck/tests/lookup/injection/non/contextual/TestFilter2.java
@@ -0,0 +1,62 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2010, Red Hat, Inc., and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jboss.cdi.tck.tests.lookup.injection.non.contextual;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletResponse;
+import javax.xml.ws.WebServiceRef;
+
+public class TestFilter2 implements Filter {
+
+    @WebServiceRef(value = TranslatorEndpointService.class)
+    Translator translatorField;
+
+    Translator translator;
+
+    static boolean initCalledAfterWSResourceInjection = false;
+
+    public void destroy() {
+    }
+
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException,
+            ServletException {
+        HttpServletResponse resp = (HttpServletResponse) response;
+
+        if (request.getParameter("test").equals("wsresource")) {
+            // Return 200 if the resource was injected before init, 500 otherwise
+            resp.setStatus(initCalledAfterWSResourceInjection ? 200 : 500);
+        } else {
+            resp.setStatus(404);
+        }
+    }
+
+    public void init(FilterConfig filterConfig) throws ServletException {
+        initCalledAfterWSResourceInjection = translator != null & translatorField != null;
+    }
+
+    @WebServiceRef(value = TranslatorEndpointService.class)
+    private void setTranslator(Translator translator) {
+        this.translator = translator;
+    }
+}
---
+++ impl/src/main/java/org/jboss/cdi/tck/tests/lookup/injection/non/contextual/TestServlet2.java
@@ -0,0 +1,63 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2010, Red Hat, Inc., and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jboss.cdi.tck.tests.lookup.injection.non.contextual;
+
+import java.io.IOException;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.xml.ws.WebServiceRef;
+
+public class TestServlet2 extends HttpServlet {
+
+    @WebServiceRef(value = TranslatorEndpointService.class)
+    Translator translatorField;
+
+    Translator translator;
+
+    static boolean initCalledAfterWSResourceInjection = false;
+
+    private static final long serialVersionUID = -7672096098457821010L;
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        if (req.getParameter("test").equals("wsresource")) {
+            // Return 200 if the resource was injected before init, 500 otherwise
+            resp.setStatus(initCalledAfterWSResourceInjection ? 200 : 500);
+        } else {
+            resp.setStatus(404);
+        }
+    }
+
+    @Override
+    public void init() throws ServletException {
+        initCalledAfterWSResourceInjection = translator != null & translatorField != null;
+    }
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        init();
+    }
+
+    @WebServiceRef(value = TranslatorEndpointService.class)
+    private void setTranslator(Translator translator) {
+        this.translator = translator;
+    }
+}
