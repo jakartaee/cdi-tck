85cba907530f5514dd291b7da0f72b8019e03f7a
Matej Briskar <mbriskar@redhat.com>
2013-11-07 13:27:31 +0100

    Check, that the visibility has no impact on the injection (using package-private superclass).

---
+++ impl/src/main/java/org/jboss/cdi/tck/tests/lookup/injection/visibility/InjectionVisibilityTest.java
@@ -0,0 +1,51 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2010, Red Hat, Inc., and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,  
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jboss.cdi.tck.tests.lookup.injection.visibility;
+
+import static org.jboss.cdi.tck.cdi.Sections.INJECTION;
+import static org.testng.Assert.assertTrue;
+
+import javax.inject.Inject;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.cdi.tck.AbstractTest;
+import org.jboss.cdi.tck.shrinkwrap.WebArchiveBuilder;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.jboss.test.audit.annotations.SpecAssertion;
+import org.jboss.test.audit.annotations.SpecVersion;
+import org.testng.annotations.Test;
+
+@SpecVersion(spec = "cdi", version = "1.1 Final Release")
+public class InjectionVisibilityTest extends AbstractTest {
+
+    @Deployment
+    public static WebArchive createTestArchive() {
+        return new WebArchiveBuilder().withTestClassPackage(InjectionVisibilityTest.class).build();
+    }
+
+    @Inject
+    SimpleSessionBean bean;
+
+    @Test
+    @SpecAssertion(section = INJECTION, id = "a")
+    public void testPackagePrivateSetMethodInjection() throws Exception {
+        bean.simpleMethod(); // because of lazy loading
+        assertTrue(AbstractBean.fooSetCalled);
+    }
+
+
+}
