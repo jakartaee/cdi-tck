4943b70eb17817e17f6cfb93fab1b1e3ea74fc98
Matus Abaffy <maabaffy@redhat.com>
2013-12-09 19:16:38 +0100

    Part of CDITCK-354 - improve coverage of section 2.7 Timeout Method Interceptors

---
+++ impl/src/main/java/org/jboss/cdi/tck/interceptors/tests/contract/aroundTimeout/Alarm.java
@@ -0,0 +1,57 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2010, Red Hat, Inc., and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jboss.cdi.tck.interceptors.tests.contract.aroundTimeout;
+
+import static org.testng.Assert.assertTrue;
+
+import javax.annotation.Resource;
+import javax.annotation.security.RolesAllowed;
+import javax.annotation.security.RunAs;
+import javax.ejb.EJB;
+import javax.ejb.SessionContext;
+import javax.ejb.Stateless;
+import javax.ejb.Timeout;
+import javax.ejb.Timer;
+import javax.interceptor.Interceptors;
+
+@Stateless
+@Interceptors(AlarmSecurityInterceptor.class)
+@RunAs("alarm")
+@RolesAllowed("student")
+public class Alarm {
+
+    public static Long timeoutAt = null;
+
+    @EJB
+    private Bell bell;
+
+    @Resource
+    private SessionContext ctx;
+
+    public void createTimer() {
+        ctx.getTimerService().createTimer(1l, null);
+    }
+
+    @Timeout
+    public void ejbTimeout(Timer timer) {
+        timeoutAt = System.currentTimeMillis();
+        assertTrue(!this.ctx.isCallerInRole("student"));
+        assertTrue(!this.ctx.isCallerInRole("alarm"));
+
+        bell.ring();
+    }
+}
