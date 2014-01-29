c2133d2997438e1983270f9b12068592a524bb6f
Martin Kouba <mkouba@redhat.com>
2014-01-29 09:57:47 +0100

    CDITCK-389 Fix InterceptorBindingInheritanceTest

--- impl/src/main/java/org/jboss/cdi/tck/tests/interceptors/definition/inheritance/WaterChestnut.java
+++ impl/src/main/java/org/jboss/cdi/tck/tests/interceptors/definition/inheritance/WaterChestnut.java
@@ -9,17 +9,17 @@
  * You may obtain a copy of the License at
  * http://www.apache.org/licenses/LICENSE-2.0
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,  
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
 package org.jboss.cdi.tck.tests.interceptors.definition.inheritance;
 
-import javax.ejb.Stateless;
+import javax.ejb.Stateful;
 
 @Culinary
-@Stateless
+@Stateful
 public class WaterChestnut extends Grass {
 
 }
