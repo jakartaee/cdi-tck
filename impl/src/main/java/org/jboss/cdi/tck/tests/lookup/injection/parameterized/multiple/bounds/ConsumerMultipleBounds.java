2248d0df14bc2eedbff51643747d57ac62b0e62a
Matus Abaffy <maabaffy@redhat.com>
2014-06-03 15:53:12 +0200

    CDITCK-414 - Create tests regarding WELD-1671 Test bean assignability (injection) regarding parameterized types with type variable(s) with multiple bounds

---
+++ impl/src/main/java/org/jboss/cdi/tck/tests/lookup/injection/parameterized/multiple/bounds/ConsumerMultipleBounds.java
@@ -0,0 +1,64 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2014, Red Hat, Inc., and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jboss.cdi.tck.tests.lookup.injection.parameterized.multiple.bounds;
+
+import javax.enterprise.inject.Any;
+import javax.enterprise.inject.Default;
+import javax.inject.Inject;
+
+class ConsumerMultipleBounds<T extends BarImpl & Baz & Foo, U extends Bar & Baz> {
+
+    @Inject
+    @Default
+    private GenericInterface<T> giBarBazFooImpl;
+
+    @Inject
+    @BarFooQualifier
+    private GenericInterface<T> giBarFooImpl;
+
+    @Inject
+    @BazQualifier
+    private GenericInterface<T> giSuperBazImpl;
+
+    @Inject
+    @Any
+    private GenericInterface<BarFooImpl> giBarFooImpl2;
+
+    @Inject
+    @Any
+    private GenericInterface<? super U> giSuperBazImpl2;
+
+    public GenericInterface<T> getGenericInterfaceBarFooImpl() {
+        return giBarFooImpl;
+    }
+
+    public GenericInterface<T> getGenericInterfaceBarBazFooImpl() {
+        return giBarBazFooImpl;
+    }
+
+    public GenericInterface<T> getGenericInterfaceSuperBazImpl() {
+        return giSuperBazImpl;
+    }
+
+    public GenericInterface<BarFooImpl> getGenericInterfaceBarFooImpl2() {
+        return giBarFooImpl2;
+    }
+
+    public GenericInterface<? super U> getGenericInterfaceSuperBazImpl2() {
+        return giSuperBazImpl2;
+    }
+}
---
+++ impl/src/main/java/org/jboss/cdi/tck/tests/lookup/injection/parameterized/multiple/bounds/ParameterizedTypesWithTypeVariableWithMultipleBoundsTest.java
@@ -0,0 +1,74 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2014, Red Hat, Inc., and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jboss.cdi.tck.tests.lookup.injection.parameterized.multiple.bounds;
+
+import static org.jboss.cdi.tck.cdi.Sections.ASSIGNABLE_PARAMETERS;
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertNotNull;
+
+import javax.inject.Inject;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.cdi.tck.AbstractTest;
+import org.jboss.cdi.tck.shrinkwrap.WebArchiveBuilder;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.jboss.test.audit.annotations.SpecAssertion;
+import org.jboss.test.audit.annotations.SpecAssertions;
+import org.jboss.test.audit.annotations.SpecVersion;
+import org.testng.annotations.Test;
+
+/**
+ * @author Matus Abaffy
+ */
+@SpecVersion(spec = "cdi", version = "1.1 Final Release")
+public class ParameterizedTypesWithTypeVariableWithMultipleBoundsTest extends AbstractTest {
+
+    @Deployment
+    public static WebArchive createTestArchive() {
+        return new WebArchiveBuilder()
+                .withTestClassPackage(ParameterizedTypesWithTypeVariableWithMultipleBoundsTest.class)
+                .build();
+    }
+
+    @Inject
+    ConsumerMultipleBounds<?, ?> consumer;
+
+    @Test
+    @SpecAssertions({ @SpecAssertion(section = ASSIGNABLE_PARAMETERS, id = "dc") })
+    public void testInjectionOfBeanWithWildcardWithTypeVariableAsLowerBound() {
+        assertNotNull(consumer.getGenericInterfaceSuperBazImpl2());
+        assertEquals(consumer.getGenericInterfaceSuperBazImpl2().getId(), GenericInterfaceSuperBazImpl.class.getSimpleName());
+    }
+
+    @Test
+    @SpecAssertions({ @SpecAssertion(section = ASSIGNABLE_PARAMETERS, id = "e") })
+    public void testInjectionOfBeanWithTypeVariableWithMultipleBoundsToParameterizedTypeWithActualType() {
+        assertNotNull(consumer.getGenericInterfaceBarFooImpl2());
+        assertEquals(consumer.getGenericInterfaceBarFooImpl2().getId(), GenericInterfaceBarFooImpl.class.getSimpleName());
+    }
+
+    @Test
+    @SpecAssertions({ @SpecAssertion(section = ASSIGNABLE_PARAMETERS, id = "f") })
+    public void testInjectionOfBeanWithTypeVariableWithMultipleBounds() {
+        assertNotNull(consumer.getGenericInterfaceBarBazFooImpl());
+        assertEquals(consumer.getGenericInterfaceBarBazFooImpl().getId(), GenericInterfaceBarBazFooImpl.class.getSimpleName());
+        assertNotNull(consumer.getGenericInterfaceBarFooImpl());
+        assertEquals(consumer.getGenericInterfaceBarFooImpl().getId(), GenericInterfaceBarFooImpl.class.getSimpleName());
+        assertNotNull(consumer.getGenericInterfaceSuperBazImpl());
+        assertEquals(consumer.getGenericInterfaceSuperBazImpl().getId(), GenericInterfaceSuperBazImpl.class.getSimpleName());
+    }
+}
