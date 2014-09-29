3c17c078d28295d49b6efbdcc69690f5d58a484a
Tomas Remes <tremes@redhat.com>
2014-09-29 15:27:43 +0200

    Parent update + require java8

--- impl/src/main/java/org/jboss/cdi/tck/tests/event/fires/FireEventTest.java
+++ impl/src/main/java/org/jboss/cdi/tck/tests/event/fires/FireEventTest.java
@@ -36,7 +36,6 @@ import javax.enterprise.context.spi.CreationalContext;
 import javax.enterprise.event.Event;
 import javax.enterprise.inject.spi.Bean;
 import javax.enterprise.inject.spi.InjectionPoint;
-import javax.enterprise.util.AnnotationLiteral;
 
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.cdi.tck.AbstractTest;
