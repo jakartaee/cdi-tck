dc02aa0f102e4819e612124e4e6ec407eb096b26
Tomas Remes <tremes@redhat.com>
2014-10-06 14:45:55 +0200

    CDITCK-452 rename targetNamespace value on WS endpoints to reflect given package
    Conflicts:
    	impl/src/main/java/org/jboss/cdi/tck/tests/context/passivating/dependency/resource/webservice/HelloWeb.java
    	impl/src/main/java/org/jboss/cdi/tck/tests/context/passivating/dependency/resource/webservice/HelloWebEndpointService.java

--- impl/src/main/java/org/jboss/cdi/tck/tests/implementation/simple/resource/broken/type/ws/TranslatorEndpointService.java
+++ impl/src/main/java/org/jboss/cdi/tck/tests/implementation/simple/resource/broken/type/ws/TranslatorEndpointService.java
@@ -24,7 +24,7 @@ import javax.xml.ws.WebEndpoint;
 import javax.xml.ws.WebServiceClient;
 import javax.xml.ws.WebServiceFeature;
 
-@WebServiceClient(name="Translator", targetNamespace = "http://ws.resources.tests.weld.jboss.org/")
+@WebServiceClient(name="Translator", targetNamespace = "http://ws.type.broken.resource.simple.implementation.tests.tck.cdi.jboss.org/")
 public class TranslatorEndpointService extends Service {
 
     public TranslatorEndpointService(URL wsdlDocumentLocation, QName serviceName) {
