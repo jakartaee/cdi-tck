248a46419dd1ff4e636b15416da89fb8251fe48c
Tomas Remes <tremes@redhat.com>
2014-10-21 12:43:04 +0200

    CDITCK-453 - Make SerializableDecorator aplicable only in given test archive.
    Conflicts:
    	impl/src/main/resources/tck-tests.xml

--- impl/src/main/java/org/jboss/cdi/tck/tests/decorators/definition/broken/nodecoratedtypes/NonExisting.java
+++ impl/src/main/java/org/jboss/cdi/tck/tests/decorators/definition/broken/nodecoratedtypes/NonExisting.java
@@ -1,6 +1,6 @@
 /*
  * JBoss, Home of Professional Open Source
- * Copyright 2012, Red Hat, Inc., and individual contributors
+ * Copyright 2014, Red Hat, Inc., and individual contributors
  * by the @authors tag. See the copyright.txt in the distribution for a
  * full listing of individual contributors.
  *
@@ -9,27 +9,26 @@
  * You may obtain a copy of the License at
  * http://www.apache.org/licenses/LICENSE-2.0
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
+ * distributed under the License is distributed on an "AS IS" BASIS,  
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
 package org.jboss.cdi.tck.tests.decorators.definition.broken.nodecoratedtypes;
 
-import java.io.Serializable;
-import javax.annotation.Priority;
-import javax.decorator.Decorator;
-import javax.decorator.Delegate;
-import javax.inject.Inject;
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
-@SuppressWarnings("serial")
-@Decorator
-@Priority(value = 10)
-public class SerializableDecorator implements Serializable {
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+import javax.inject.Qualifier;
 
-    @SuppressWarnings("unused")
-    @Inject
-    @NonExisting
-    @Delegate
-    private Serializable delegate;
+@Target({ TYPE, FIELD })
+@Retention(RUNTIME)
+@Documented
+@Qualifier
+public @interface NonExisting {
+
 }
