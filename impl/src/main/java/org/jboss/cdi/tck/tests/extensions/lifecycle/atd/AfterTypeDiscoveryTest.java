9c60fdbfbd30c6a4a7bc3d97053fed2f98c904e7
Tomas Remes <tremes@redhat.com>
2015-01-05 12:35:35 +0100

    CDITCK-460 remove test group

--- impl/src/main/java/org/jboss/cdi/tck/tests/extensions/lifecycle/atd/AfterTypeDiscoveryTest.java
+++ impl/src/main/java/org/jboss/cdi/tck/tests/extensions/lifecycle/atd/AfterTypeDiscoveryTest.java
@@ -16,7 +16,6 @@
  */
 package org.jboss.cdi.tck.tests.extensions.lifecycle.atd;
 
-import static org.jboss.cdi.tck.TestGroups.INTEGRATION;
 import static org.jboss.cdi.tck.cdi.Sections.ATD;
 import static org.jboss.cdi.tck.cdi.Sections.BEAN_DISCOVERY;
 import static org.jboss.cdi.tck.cdi.Sections.PP;
@@ -47,7 +46,6 @@ import org.testng.annotations.Test;
  * @author Martin Kouba
  */
 @SpecVersion(spec = "cdi", version = "1.1 Final Release")
-@Test(groups = INTEGRATION)
 public class AfterTypeDiscoveryTest extends AbstractTest {
 
     @Deployment
