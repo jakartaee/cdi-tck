468ab90175b6927e97250cbcd0b9ff6e280abe07
Jozef Hartinger <jharting@redhat.com>
2015-01-19 12:38:53 +0100

    Make sure interceptor recovery is tested properly

--- impl/src/main/java/org/jboss/cdi/tck/interceptors/tests/contract/exceptions/aroundInvoke/Interceptor4.java
+++ impl/src/main/java/org/jboss/cdi/tck/interceptors/tests/contract/exceptions/aroundInvoke/Interceptor4.java
@@ -9,7 +9,7 @@
  * You may obtain a copy of the License at
  * http://www.apache.org/licenses/LICENSE-2.0
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,  
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
@@ -23,6 +23,6 @@ public class Interceptor4 {
     @AroundInvoke
     public Object intercept(InvocationContext ctx) throws Exception {
         ExceptionBean.failFirstTwoInvocations();
-        return ctx.proceed();
+        return ! (Boolean) ctx.proceed();
     }
 }
