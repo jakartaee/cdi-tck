6c13cceaef751dbe1b3733c9c6305ca2c899dead
Tomas Remes <tremes@redhat.com>
2015-05-18 10:27:28 +0200

    CDITCK-482 add session.invalidate() call.

--- impl/src/main/java/org/jboss/cdi/tck/tests/context/session/listener/shutdown/InitServlet.java
+++ impl/src/main/java/org/jboss/cdi/tck/tests/context/session/listener/shutdown/InitServlet.java
@@ -45,5 +45,6 @@ public class InitServlet extends HttpServlet {
         } else {
             response.setStatus(HttpServletResponse.SC_NOT_FOUND);
         }
+        request.getSession().invalidate();
     }
 }
