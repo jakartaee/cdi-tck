7e74b5ede02da0e372b470bd5b729005ee2d7eca
Tomas Remes <tremes@redhat.com>
2015-07-29 10:41:47 +0200

    Oops version update.

--- impl/src/main/java/org/jboss/cdi/tck/tests/event/observer/context/enterprise/SessionBeanObserverMethodInvocationContextTest.java
+++ impl/src/main/java/org/jboss/cdi/tck/tests/event/observer/context/enterprise/SessionBeanObserverMethodInvocationContextTest.java
@@ -35,7 +35,7 @@ import org.testng.annotations.Test;
 /**
  * @author Matus Abaffy
  */
-@SpecVersion(spec = "cdi", version = "1.1 Final Release")
+@SpecVersion(spec = "cdi", version = "2.0-EDR1")
 public class SessionBeanObserverMethodInvocationContextTest extends AbstractTest {
 
     @Deployment
