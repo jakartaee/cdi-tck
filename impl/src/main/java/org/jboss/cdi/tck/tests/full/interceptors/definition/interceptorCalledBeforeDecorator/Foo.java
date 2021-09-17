26868b9782f54ca1af2807981154603cb3456557
Scott M Stark <starksm64@gmail.com>
2021-09-17 00:36:49 -0500

    Move cdi-full group tests to under org.jboss.cdi.tck.tests.full.interceptors.*
    Signed-off-by: Scott M Stark <starksm64@gmail.com>

---
+++ impl/src/main/java/org/jboss/cdi/tck/tests/full/interceptors/definition/interceptorCalledBeforeDecorator/Foo.java
@@ -0,0 +1,7 @@
+package org.jboss.cdi.tck.tests.full.interceptors.definition.interceptorCalledBeforeDecorator;
+
+public interface Foo {
+
+    public abstract void bar();
+
+}
--- impl/src/main/java/org/jboss/cdi/tck/tests/interceptors/definition/interceptorCalledBeforeDecorator/Foo.java
+++
@@ -1,7 +0,0 @@
-package org.jboss.cdi.tck.tests.interceptors.definition.interceptorCalledBeforeDecorator;
-
-public interface Foo {
-
-    public abstract void bar();
-
-}
