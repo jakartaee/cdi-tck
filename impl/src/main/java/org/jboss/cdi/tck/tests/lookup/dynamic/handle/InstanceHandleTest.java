4afe5babc631fb7dcdfee63134e99e1825d54bb5
Matej Novotny <manovotn@redhat.com>
2021-12-08 11:49:22 +0100

    Correction to InstanceHandleTest in order to align it with latest changes in CDI API.

--- impl/src/main/java/org/jboss/cdi/tck/tests/lookup/dynamic/handle/InstanceHandleTest.java
+++ impl/src/main/java/org/jboss/cdi/tck/tests/lookup/dynamic/handle/InstanceHandleTest.java
@@ -128,7 +128,7 @@ public class InstanceHandleTest extends AbstractTest {
 
         ActionSequence.reset();
         assertTrue(instance.isAmbiguous());
-        for (Iterator<Instance.Handle<Processor>> iterator = instance.handles().iterator(); iterator.hasNext(); ) {
+        for (Iterator<? extends Instance.Handle<Processor>> iterator = instance.handles().iterator(); iterator.hasNext(); ) {
             try (Instance.Handle<Processor> handle = iterator.next()) {
                 handle.get().ping();
             }
