470d732c7c94f6b42335a537cf25c748ba796080
Ladislav Thon <lthon@redhat.com>
2023-04-26 12:09:25 +0200

    remove unused elements of MockCreationalContext

--- impl/src/main/java/org/jboss/cdi/tck/util/MockCreationalContext.java
+++ impl/src/main/java/org/jboss/cdi/tck/util/MockCreationalContext.java
@@ -16,60 +16,38 @@
  */
 package org.jboss.cdi.tck.util;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import jakarta.enterprise.context.spi.CreationalContext;
 
 public class MockCreationalContext<T> implements CreationalContext<T> {
-    private static List<Object> beansPushed = new ArrayList<Object>();
-    private static Object lastBeanPushed = null;
     private static boolean pushCalled = false;
+    private static Object lastBeanPushed = null;
     private static boolean releaseCalled = false;
 
     public void push(T incompleteInstance) {
         pushCalled = true;
         lastBeanPushed = incompleteInstance;
-        beansPushed.add(incompleteInstance);
     }
 
-    public static Object getLastBeanPushed() {
-        return lastBeanPushed;
-    }
-
-    public static List<Object> getBeansPushed() {
-        return beansPushed;
-    }
-
-    public static void setLastBeanPushed(Object lastBeanPushed) {
-        MockCreationalContext.lastBeanPushed = lastBeanPushed;
+    public void release() {
+        releaseCalled = true;
     }
 
     public static boolean isPushCalled() {
         return pushCalled;
     }
 
-    public static void setPushCalled(boolean pushCalled) {
-        MockCreationalContext.pushCalled = pushCalled;
+    public static Object getLastBeanPushed() {
+        return lastBeanPushed;
     }
 
     public static boolean isReleaseCalled() {
         return releaseCalled;
     }
 
-    public static void setReleaseCalled(boolean releaseCalled) {
-        MockCreationalContext.releaseCalled = releaseCalled;
-    }
-
     public static void reset() {
         lastBeanPushed = null;
-        beansPushed.clear();
         pushCalled = false;
         releaseCalled = false;
-    }
-
-    public void release() {
-        releaseCalled = true;
     }
 
 }
