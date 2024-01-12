466f0f236a533973a15086b5f58c1b130d508b43
Andrew Rouse <anrouse@uk.ibm.com>
2024-01-12 18:03:54 +0000

    Test BeanContainer assignability API methods
    Tests for .isMatchingBean and .isMatchingEvent.

---
+++ impl/src/main/java/org/jboss/cdi/tck/tests/beanContainer/assignability/MyEvent.java
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2024 Contributors to the Eclipse Foundation
+ *
+ * This program and the accompanying materials are made available under the
+ * Apache Software License 2.0 which is available at:
+ * https://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+package org.jboss.cdi.tck.tests.beanContainer.assignability;
+
+public class MyEvent implements MyEventInterface {
+
+}
---
+++ impl/src/main/java/org/jboss/cdi/tck/tests/beanContainer/assignability/Qualifier1.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2024 Contributors to the Eclipse Foundation
+ *
+ * This program and the accompanying materials are made available under the
+ * Apache Software License 2.0 which is available at:
+ * https://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+package org.jboss.cdi.tck.tests.beanContainer.assignability;
+
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.PARAMETER;
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import jakarta.enterprise.util.AnnotationLiteral;
+import jakarta.inject.Qualifier;
+
+@Qualifier
+@Retention(RUNTIME)
+@Target({ TYPE, METHOD, FIELD, PARAMETER })
+public @interface Qualifier1 {
+    
+    static class Literal extends AnnotationLiteral<Qualifier1> implements Qualifier1 {
+        static final Qualifier1 INSTANCE = new Literal();
+    }
+}
