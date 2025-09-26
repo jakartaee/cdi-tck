a5c0b2bd977753761771c9e3bf50145b537f278f
Matej Novotny <manovotn@ibm.com>
2025-09-26 05:19:43 +0200

    Add TCK coverage for wildcard in Event/Injection injection points

---
+++ impl/src/main/java/org/jboss/cdi/tck/tests/lookup/dynamic/broken/wildcard/InstanceWithWildcardTest.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2025, Contributors to the Eclipse Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.jboss.cdi.tck.tests.lookup.dynamic.broken.wildcard;
+
+import jakarta.enterprise.inject.spi.DefinitionException;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.container.test.api.ShouldThrowException;
+import org.jboss.cdi.tck.AbstractTest;
+import org.jboss.cdi.tck.cdi.Sections;
+import org.jboss.cdi.tck.shrinkwrap.WebArchiveBuilder;
+import org.jboss.cdi.tck.tests.event.broken.wildcard.EventWithWildcardTest;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.jboss.test.audit.annotations.SpecAssertion;
+import org.jboss.test.audit.annotations.SpecVersion;
+import org.testng.annotations.Test;
+
+@SpecVersion(spec = "cdi", version = "5.0")
+public class InstanceWithWildcardTest extends AbstractTest {
+
+    @ShouldThrowException(DefinitionException.class)
+    @Deployment
+    public static WebArchive createTestArchive() {
+        return new WebArchiveBuilder().withTestClass(EventWithWildcardTest.class)
+                .withClasses(BeanWithWildcardInstance.class).build();
+    }
+
+    @Test
+    @SpecAssertion(section = Sections.DYNAMIC_LOOKUP, id = "ca")
+    public void testEventWildcardIsDetected() {
+        // test should throw an exception
+    }
+}
