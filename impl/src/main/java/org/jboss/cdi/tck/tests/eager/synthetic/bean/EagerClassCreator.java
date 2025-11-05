8f6568b69aa1db05da1ad505abb4437b4902251b
Ladislav Thon <lthon@redhat.com>
2025-11-05 11:59:35 +0100

    add TCK tests for eager initialization (`@Eager`)

---
+++ impl/src/main/java/org/jboss/cdi/tck/tests/eager/synthetic/bean/EagerClassCreator.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) 2025 Contributors to the Eclipse Foundation
+ *
+ * This program and the accompanying materials are made available under the
+ * Apache Software License 2.0 which is available at:
+ * https://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+package org.jboss.cdi.tck.tests.eager.synthetic.bean;
+
+import jakarta.enterprise.inject.build.compatible.spi.Parameters;
+import jakarta.enterprise.inject.build.compatible.spi.SyntheticBeanCreator;
+import jakarta.enterprise.inject.build.compatible.spi.SyntheticInjections;
+
+public class EagerClassCreator implements SyntheticBeanCreator<EagerClass> {
+    static boolean created = false;
+
+    @Override
+    public EagerClass create(SyntheticInjections lookup, Parameters params) {
+        created = true;
+        return new EagerClass();
+    }
+}
