56fec14655f9cd774edfe5ca4442c8a078cc76dc
Ladislav Thon <lthon@redhat.com>
2026-01-20 11:41:30 +0100

    improve tests for custom contexts
    1. Tests for registering custom contexts for global built-in scopes are added,
       both for build compatible extensions and portable extensions. These tests
       verify that attempting to register a custom context for the application and
       dependent scopes lead to a deployment problem.
    2. Test for registering custom context for the request scope is added
       for build compatible extensions (already existed for portable extensions).
       Registering custom contexts for built-in scopes was previously not allowed
       by the specification of build compatible extensions, but wasn't enforced.
       It is allowed now, and this test verifies that it works.

---
+++ impl/src/main/java/org/jboss/cdi/tck/tests/full/context/CustomApplicationContextTest.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright (c) 2026 Contributors to the Eclipse Foundation
+ *
+ * This program and the accompanying materials are made available under the
+ * Apache Software License 2.0 which is available at:
+ * https://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+package org.jboss.cdi.tck.tests.full.context;
+
+import static org.jboss.cdi.tck.TestGroups.CDI_FULL;
+import static org.jboss.cdi.tck.cdi.Sections.BUILTIN_CONTEXTS;
+
+import jakarta.enterprise.inject.spi.DeploymentException;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.container.test.api.ShouldThrowException;
+import org.jboss.cdi.tck.AbstractTest;
+import org.jboss.cdi.tck.shrinkwrap.WebArchiveBuilder;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.jboss.test.audit.annotations.SpecAssertion;
+import org.jboss.test.audit.annotations.SpecVersion;
+import org.testng.annotations.Test;
+
+@SpecVersion(spec = "cdi", version = "5.0")
+public class CustomApplicationContextTest extends AbstractTest {
+    @Deployment
+    @ShouldThrowException(DeploymentException.class)
+    public static WebArchive createTestArchive() {
+        return new WebArchiveBuilder()
+                .withTestClassPackage(CustomApplicationContextTest.class)
+                .withExtension(CustomApplicationContextExtension.class)
+                .build();
+    }
+
+    @Test(groups = CDI_FULL)
+    @SpecAssertion(section = BUILTIN_CONTEXTS, id = "aa")
+    public void testCustomApplicationContextLeadsToDeploymentProblem() {
+    }
+}
